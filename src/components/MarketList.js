935c0520a27be619b154310104fc0ce2e5f8c294
Anzgal <agalvan70@hotmail.com>
2020-11-24 16:59:41 -0600

    test2

--- src/components/MarketList.js
+++ src/components/MarketList.js
@@ -8,113 +8,77 @@ import {Link} from 'react-router-dom';
 
 import { Loading, Card, Icon, Tag } from "element-react";
 
-const MarketList = ({ term, searchResults }) => {
-	return (
-		<Connect
-			query={graphqlOperation(listMarkets)}
-			subscription={graphqlOperation(onCreateMarket)}
-			onSubscriptionMsg={(prevList, updatedItem) => {
-				// 注意：这里的prevList其实是data部分，不包含loading和errors
-				// shallow copy here
-				let updatedList = { ...prevList };
-				const updatedItems = [ updatedItem.onCreateMarket, ...prevList.listMarkets.items ];
-				updatedList.listMarkets.items = updatedItems;
-				return updatedList;
-			}}
-		>
-			{({ data, loading, errors }) => {
-				if (errors.length > 0) return <Error errors={errors} />;
-				if (loading || !data.listMarkets) return <Loading fullscreen={true} />;
-				let markets;
-				let resDisplay;
+const MarketList = ({searchResults}) => {
+  const onNewMarket = (prevQuery, newData) => {
+    let updatedQuery = {...prevQuery};
+    const updatedMarketList = [
+      newData.onCreateMarket,
+      ...prevQuery.listMarkets.items
+    ]
+    updatedQuery.listMarkets.items = updatedMarketList;
+    return updatedQuery;
+  }
+  return (
+    <Connect query={graphqlOperation(listMarkets)} subscription={graphqlOperation(onCreateMarket)} onSubscriptionMsg={onNewMarket} >
 
-				if (term === '') {
-					markets = data.listMarkets.items;
-					resDisplay = (
-						<h2 className="header">
-							<img
-								src="https://icon.now.sh/store_mall_directory/527FFF"
-								alt="Store Icon"
-								className="large-icon"
-							/>
-							Markets
-						</h2>
-					);
-				} else if (searchResults.length > 0) {
-					markets = searchResults;
-					resDisplay = (
-						<h2 className="text-green">
-							<Icon type="success" name="check" className="icon" />
-							{searchResults.length} {searchResults.length === 1 ? 'result' : 'results'} for
-							{' "' + term + '"'}
-						</h2>
-					);
-				} else {
-					markets = [];
-					resDisplay = (
-						<h2 className="text-green">
-							<Icon type="success" name="check" className="icon" />
-							0 result for
-							{' "' + term + '"'}
-						</h2>
-					);
-				}
+      {
+        ({data, loading, errors }) => {
 
-				return (
-					<React.Fragment>
-						{resDisplay}
+          if (errors.length > 0) return <Error errors={errors} />
+          if (loading || !data.listMarkets) return <Loading fullscreen={true}/>
+          
+          const markets = searchResults.length > 0 ? searchResults : data.listMarkets.items;
 
-						{markets.map((market) => {
-							return (
-								<div key={market.id} className="my-2">
-									{/* Display as Card Component */}
-									<Card
-										bodyStyle={{
-											padding: '0.7em',
-											display: 'flex',
-											alighItems: 'center',
-											justifyContent: 'space-between'
-										}}
-									>
-										<div>
-											{/* 这是一行 */}
-											<span className="flex">
-												{/* Market name  */}
-												<Link className="link" to={`/markets/${market.id}`}>
-													{market.name}
-												</Link>
+          return(
+            <React.Fragment>
+              {searchResults.length > 0 ? (
+                <h2 className="text-green">
+                  <Icon type="success" name="check" className="icon"></Icon>
+                  {searchResults.length} Resultados
+                </h2>
+              ) : ( <h2 className="header">
+                <img src="https://www.flaticon.es/svg/static/icons/svg/3752/3752599.svg" alt="directory" className="large-icon"/>
+                Changarros
+              </h2>)}
+              {markets.map(market => (
+                <div key={market.id} className="my-2">
+                  <Card bodyStyle={{padding: "0.7em", display:"flex",alignItems: "center", justifyContent: "space-between" }}>
+                    <div>
+                      <span className="flex">
+                        <Link className="link" to={`/markets/${market.id}`}>
+                          {market.name}
+                        </Link>
 
-												{/* Number of Products*/}
-												<span style={{ color: 'var(--darkAmazonOrange)' }}>
-													{market.products.items ? market.products.items.length : 0}
-												</span>
+                        <span style={{color: 'var(--darkAmazonOrange)'}}>
+                          {market.products.items ? market.products.items.length : 0}
+                        </span>
 
-												{/* Shopping Cart */}
-												<img src="https://icon.now.sh/shopping_cart/f60" alt="Shopping Cart" />
-											</span>
+                        <img src='https://www.flaticon.es/svg/static/icons/svg/891/891462.svg' alt="shopping cart" className="large-icon"/>
 
-											{/* Market Owner 换到下一行 */}
-											<div style={{ color: 'var(--lightSquidInk)' }}>{market.owner}</div>
-										</div>
+                      </span>
+                      <div style={{color: "var(--lightSquidInk)"}}>
+                        {market.owner}
+                      </div>
 
-										{/* Market tags */}
-										<div>
-											{market.tags &&
-												market.tags.map((tag) => (
-													<Tag key={tag} type="danger" className="mx-1">
-														{tag}
-													</Tag>
-												))}
-										</div>
-									</Card>
-								</div>
-							);
-						})}
-					</React.Fragment>
-				);
-			}}
-		</Connect>
-	);
+                    </div>
+                      <div>
+                      {market.tags && market.tags.map(tag => (
+                        <Tag key={tag} type="danger" className="mx-1">
+                          {tag}
+                        </Tag>
+                      ))}
+                      </div>
+                    
+                  </Card>
+                </div>
+              ))}
+            </React.Fragment>
+          );
+        }
+      }
+
+    </Connect>
+  );
 };
 
 export default MarketList;
